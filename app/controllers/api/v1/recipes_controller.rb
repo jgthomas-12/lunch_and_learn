2fff16e675ea94b83e0c26847ad30ec841b42677
Jesse Thomas <jgthomas3@gmail.com>
2023-08-06 13:09:55 -0600

    Feat: Update RecipesController

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    country = params[:country]
-    recipes = RecipesFacade.new.get_recipes(country)
+    if params[:country] == ""
+      country = CountriesFacade.new.get_country
+      recipes = RecipesFacade.new.get_recipes(country.name)
 
-    render json: RecipeSerializer.new(recipes)
+      render json: RecipeSerializer.new(recipes)
+    else
+      country = params[:country]
+      recipes = RecipesFacade.new.get_recipes(country)
+
+      render json: RecipeSerializer.new(recipes)
+    end
   end
 end
