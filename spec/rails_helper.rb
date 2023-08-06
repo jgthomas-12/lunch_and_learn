a23012492f4b26cd95dd2db678f417e10ed1988c
Jesse Thomas <jgthomas3@gmail.com>
2023-08-06 13:15:58 -0600

    Fixtures and spaces

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,10 +71,11 @@ end
 # VCR.configure do |config|
 #   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
 #   config.hook_into :webmock
-#   config.filter_sensitive_data('EDAMAMS_ID') { ENV['EDAMAMS_ID'] }
+#   # config.filter_sensitive_data('ID') { ENV['EDAMAMS_ID'] }
 #   config.filter_sensitive_data('KEY') { ENV['EDAMAMS_KEY'] }
 #   config.default_cassette_options = { re_record_interval: 7.days }
 #   config.configure_rspec_metadata!
+#   # config.allow_http_connections_when_no_casettes = true
 # end
 
 Shoulda::Matchers.configure do |config|
