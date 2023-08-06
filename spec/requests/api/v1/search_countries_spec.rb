67a7bdde5e84be0b48bad9ab5a66af3143cba214
Jesse Thomas <jgthomas3@gmail.com>
2023-08-06 13:10:55 -0600

    Test: Recipes Request and sad path

--- spec/requests/api/v1/search_countries_spec.rb
+++ spec/requests/api/v1/search_countries_spec.rb
@@ -2,39 +2,110 @@ require "rails_helper"
 
 RSpec.describe "API V1 Recipes request", type: :request do
   describe "GET /api/v1/recipes" do
-    it "returns a list of recipes from a specified country" do
-      thai_response = File.read("spec/fixtures/thailand_recipes.json")
-      stub_request(:get, "https://api.edamam.com/recipes/v2/?app_id=043cfb46&app_key=e34840b26bcec722562c1fbb2d1d6e43&q=thailand&type=public")
-      .to_return(status: 200, body: thai_response)
+    describe "happy path" do
 
-      get '/api/v1/recipes', params: { country: 'thailand' }, headers: { 'Accept' => 'application/json' }
+      it "returns a list of recipes from a specified country" do
+        thai_response = File.read("spec/fixtures/thailand_recipes.json")
+        stub_request(:get, "https://api.edamam.com/recipes/v2/?app_id=043cfb46&app_key=e34840b26bcec722562c1fbb2d1d6e43&q=thailand&type=public")
+        .to_return(status: 200, body: thai_response)
 
-      # get "/api/v1/recipes?country=thailand"
+        get '/api/v1/recipes', params: { country: 'thailand' }, headers: { 'Accept' => 'application/json' }
 
-      recipes = JSON.parse(response.body, symbolize_names: true)
+        recipes = JSON.parse(response.body, symbolize_names: true)
 
-      recipes[:data].each do |recipe|
-        expect(recipe).to have_key(:id)
-        expect(recipe[:id]).to be_a(String)
-        expect(recipe[:id]).to eq("null")
+        recipes[:data].each do |recipe|
+          expect(recipe).to have_key(:id)
+          expect(recipe[:id]).to be_a(String)
+          expect(recipe[:id]).to eq("null")
 
-        expect(recipe).to have_key(:type)
-        expect(recipe[:type]).to be_a(String)
-        expect(recipe[:type]).to eq("recipe")
+          expect(recipe).to have_key(:type)
+          expect(recipe[:type]).to be_a(String)
+          expect(recipe[:type]).to eq("recipe")
 
-        expect(recipe[:attributes]).to have_key(:id)
-        expect(recipe[:attributes][:id]).to be_a(String)
-        expect(recipe[:attributes][:id]).to eq("null")
+          expect(recipe[:attributes]).to have_key(:id)
+          expect(recipe[:attributes][:id]).to be_a(String)
+          expect(recipe[:attributes][:id]).to eq("null")
 
-        expect(recipe[:attributes]).to have_key(:title)
-        expect(recipe[:attributes]).to have_key(:url)
-        expect(recipe[:attributes]).to have_key(:country)
-        expect(recipe[:attributes]).to have_key(:image_url)
+          expect(recipe[:attributes]).to have_key(:title)
+          expect(recipe[:attributes]).to have_key(:url)
+          expect(recipe[:attributes]).to have_key(:country)
+          expect(recipe[:attributes]).to have_key(:image_url)
 
-        expect(recipe[:attributes][:title]).to be_a(String)
-        expect(recipe[:attributes][:url]).to be_a(String)
-        expect(recipe[:attributes][:country]).to be_a(String)
-        expect(recipe[:attributes][:image_url]).to be_a(String)
+          expect(recipe[:attributes][:title]).to be_a(String)
+          expect(recipe[:attributes][:url]).to be_a(String)
+          expect(recipe[:attributes][:country]).to be_a(String)
+          expect(recipe[:attributes][:image_url]).to be_a(String)
+
+          expect(recipe[:attributes]).to_not have_key(:yield)
+          expect(recipe[:attributes]).to_not have_key(:dietLabels)
+          expect(recipe[:attributes]).to_not have_key(:heathLabels)
+        end
+      end
+
+      it "returns a list of recipes from a random country if no country is specified" do
+        countries = File.read("spec/fixtures/all_countries.json")
+        stub_request(:get, "https://restcountries.com/v3.1/all")
+        .to_return(status: 200, body: countries)
+
+        recipes = File.read("spec/fixtures/thailand_recipes.json")
+        stub_request(:get, "https://api.edamam.com/recipes/v2/?app_id=043cfb46&app_key=e34840b26bcec722562c1fbb2d1d6e43&q=&type=public")
+        .to_return(status: 200, body: recipes)
+
+        thai_recipes = File.read("spec/fixtures/thailand_recipes.json")
+        stub_request(:get, "https://api.edamam.com/recipes/v2/?app_id=043cfb46&app_key=e34840b26bcec722562c1fbb2d1d6e43&q=thailand&type=public")
+        .to_return(status: 200, body: recipes)
+
+        data = {
+          name: {
+            common: "thailand"
+          }
+        }
+
+        thailand = Country.new(data)
+        allow_any_instance_of(CountriesFacade).to receive(:get_country).and_return(thailand)
+
+        get '/api/v1/recipes', params: { country: '' }, headers: { 'Accept' => 'application/json' }
+
+        recipes = JSON.parse(response.body, symbolize_names: true)
+
+        recipes[:data].each do |recipe|
+          expect(recipe).to have_key(:id)
+          expect(recipe[:id]).to be_a(String)
+          expect(recipe[:id]).to eq("null")
+
+          expect(recipe).to have_key(:type)
+          expect(recipe[:type]).to be_a(String)
+          expect(recipe[:type]).to eq("recipe")
+
+          expect(recipe[:attributes]).to have_key(:id)
+          expect(recipe[:attributes][:id]).to be_a(String)
+          expect(recipe[:attributes][:id]).to eq("null")
+
+          expect(recipe[:attributes]).to have_key(:title)
+          expect(recipe[:attributes]).to have_key(:url)
+          expect(recipe[:attributes]).to have_key(:country)
+          expect(recipe[:attributes]).to have_key(:image_url)
+
+          expect(recipe[:attributes][:title]).to be_a(String)
+          expect(recipe[:attributes][:url]).to be_a(String)
+          expect(recipe[:attributes][:country]).to be_a(String)
+          expect(recipe[:attributes][:image_url]).to be_a(String)
+        end
+      end
+    end
+
+    describe "sad path" do
+      it "returns an emptry array if parameter is emptry string or value does't return any recipes" do
+        countries = File.read("spec/fixtures/empty_array.json")
+        stub_request(:get, "https://api.edamam.com/recipes/v2/?app_id=043cfb46&app_key=e34840b26bcec722562c1fbb2d1d6e43&q=dsf34&type=public")
+        .to_return(status: 200, body: countries)
+
+        get '/api/v1/recipes', params: { country: 'dsf34' }, headers: { 'Accept' => 'application/json' }
+
+        recipes = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq({ "data" => [] })
       end
     end
   end
