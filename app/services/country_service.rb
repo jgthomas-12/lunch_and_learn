8302401b73f02495f01172447b1f6310e0fdda3e
Jesse Thomas <jgthomas3@gmail.com>
2023-08-06 13:13:45 -0600

    Feat: CountryService

---
+++ app/services/country_service.rb
@@ -0,0 +1,15 @@
+class CountryService
+  def conn
+    Faraday.new(url: "https://restcountries.com")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_random_country
+    countries = get_url("/v3.1/all")
+    countries.sample
+  end
+end
