6ccabe06434a170ba97f1f36e8715362c01c28f8
Jesse Thomas <jgthomas3@gmail.com>
2023-08-07 11:13:47 -0600

    Test: AirQuality poro

---
+++ spec/poros/air_quality_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe AirQuality do
+  it "exists and has attributes" do
+    data = {
+      overall_aqi: 27,
+      "PM2.5": { concentration: 8.45 },
+      CO: { concentration: 547.41 }
+    }
+    city = "Abuja"
+
+    air_quality = AirQuality.new(data, city)
+
+    expect(air_quality).to be_a(AirQuality)
+    expect(air_quality.id).to eq("null")
+    expect(air_quality.city).to eq(city)
+    expect(air_quality.aqi).to eq(data[:overall_aqi])
+    expect(air_quality.pm25_concentration).to eq(data[:"PM2.5"][:concentration])
+    expect(air_quality.co_concentration).to eq(data[:CO][:concentration])
+  end
+end
