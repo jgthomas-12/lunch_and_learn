7163ff05e8592307cfce5f57f11584627e72ec40
Jesse Thomas <jgthomas3@gmail.com>
2023-08-08 10:28:18 -0600

    Test: users request, happy/sad

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -2,15 +2,44 @@ require "rails_helper"
 
 RSpec.describe "API V1 Users", type: :request do
   describe "POST /api/v1/users" do
-    it "creates a new user with unique api key" do
-      valid_params = {
-        name: "Odell",
-        email: "goodboy@ruffruff.com",
-        password: "treats4lyf",
-        password_confirmation: "treats4lyf"
-      }
+    describe "happy path" do
+      it "creates a new user with unique api key" do
+        valid_params = {
+          user: {
+            name: "Odell",
+            email: "goodboy@ruffruff.com",
+            password: "treats4lyf",
+            password_confirmation: "treats4lyf"
+          }
+        }
 
-      post "/api/v1/users", params: valid_params, as: :json 
+        post "/api/v1/users", params: valid_params, as: :json
+
+        user = User.first
+        expect(response.status).to eq(201)
+        expect(User.count).to eq(1)
+        expect(user.name).to eq(valid_params[:user][:name])
+        expect(user.email).to eq(valid_params[:user][:email])
+        expect(user.api_key).to be_present
+        expect(user.password_digest).to be_present
+      end
+    end
+
+    describe "sad path" do
+      it "returns an error message with invalid params" do
+        invalid_params = {
+          name: "Odell",
+          email: "goodboy@ruffruff.com",
+          password: "treats4lyf",
+          password_confirmation: "nofuggintreats"
+        }
+
+        post "/api/v1/users", params: invalid_params, as: :json
+
+        expect(response).not_to be_successful
+        expect(response.status).to eq(422)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
