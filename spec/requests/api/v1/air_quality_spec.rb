b86df0e84da55495b19755d54a29817eca100a79
Jesse Thomas <jgthomas3@gmail.com>
2023-08-07 21:48:59 -0600

    Test: extension 1 air_quality

--- spec/requests/api/v1/air_quality_spec.rb
+++ spec/requests/api/v1/air_quality_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe "API V1 Air Quality request", type: :request do
 
         air_quality = JSON.parse(response.body, symbolize_names: true)
 
+        expect(response).to be_successful
+
         expect(air_quality).to have_key(:data)
         expect(air_quality[:data]).to be_a(Hash)
 
@@ -41,6 +43,8 @@ RSpec.describe "API V1 Air Quality request", type: :request do
         get '/api/v1/air_quality', params: { country: 'nigeria' }, headers: { 'Accept' => 'application/json' }
 
         air_quality = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
 
         expect(air_quality).to have_key(:data)
         expect(air_quality[:data]).to be_a(Hash)
@@ -75,6 +79,7 @@ RSpec.describe "API V1 Air Quality request", type: :request do
 
         air_quality = JSON.parse(response.body, symbolize_names: true)
 
+        expect(response).to be_successful
         expect(air_quality).to have_key(:data)
         expect(air_quality[:data]).to be_a(Hash)
 
@@ -98,6 +103,31 @@ RSpec.describe "API V1 Air Quality request", type: :request do
         expect(air_quality[:data][:attributes][:aqi]).to be_a(Integer)
         expect(air_quality[:data][:attributes][:pm25_concentration]).to be_a(Float)
         expect(air_quality[:data][:attributes][:co_concentration]).to be_a(Float)
+      end
+    end
+
+    describe "extension 1" do
+      xit "returns air quality object for random country when no country is specified" do
+        allow_any_instance_of(CountriesFacade).to receive(:get_country).and_return("France")
+        allow_any_instance_of(AirQualityFacade).to receive(:air_quality).with("France").and_return(air_quality_params)
+
+        get '/api/v1/air_quality', params: { country: '' }, headers: { 'Accept' => 'application/json' }
+
+        air_quality = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+
+        expect(json_response['data']).to be_present
+        expect(json_response['data']['type']).to eq('air_quality')
+      end
+
+      def air_quality_params
+        {
+          city: "France",
+          aqi: 27,
+          pm25_concentration: 8.45,
+          co_concentration: 547.41
+        }
       end
     end
   end
