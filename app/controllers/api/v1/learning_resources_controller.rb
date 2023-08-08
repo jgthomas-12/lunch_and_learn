5a498322a5335da060d32b93219c2f4703bcb350
Jesse Thomas <jgthomas3@gmail.com>
2023-08-07 21:51:26 -0600

    Feat: Learning resources controller adjustments

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::LearningResourcesController < ApplicationController
   def index
     country = params[:country]
-    # resources = LearningFacade.new.get_resources(country)
-    # video = VideoFacade.new.get_video(country)
-    photos = PhotosFacade.new.get_photos(country)
+    resource = LearningResourcesFacade.new.learning_resource(country)
+
+    render json: LearningResourceSerializer.new(resource)
   end
 end
