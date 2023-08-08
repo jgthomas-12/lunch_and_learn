d154b62c346c27cd8f3f0435901f5776f0f29f92
Jesse Thomas <jgthomas3@gmail.com>
2023-08-08 16:14:05 -0600

    Feat: Add favorites route

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
 
       resources :sessions, only: [:create]
+
+      resources :favorites, only: [:create]
     end
   end
 end
