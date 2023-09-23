21c2c49fb4023ce284fc08a753cc1954fe6df0b9
Jesse Thomas <jgthomas3@gmail.com>
2023-09-23 13:13:15 -0600

    Refact: Add structure assertions

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -14,12 +14,34 @@ RSpec.describe "API V1 Users", type: :request do
         post api_v1_users_path, params: valid_params, as: :json
 
         user = User.first
+        user_response = JSON.parse(response.body, symbolize_names: true)
+
         expect(response.status).to eq(201)
         expect(User.count).to eq(1)
         expect(user.name).to eq(valid_params[:name])
         expect(user.email).to eq(valid_params[:email])
         expect(user.api_key).to be_present
         expect(user.password_digest).to be_present
+
+        expect(user_response).to have_key(:data)
+        expect(user_response[:data]).to be_a(Hash)
+
+        expect(user_response[:data]).to have_key(:id)
+        expect(user_response[:data][:id]).to be_a(String)
+
+        expect(user_response[:data]).to have_key(:type)
+        expect(user_response[:data][:type]).to be_a(String)
+
+        expect(user_response[:data]).to have_key(:attributes)
+        expect(user_response[:data][:attributes]).to be_a(Hash)
+
+        expect(user_response[:data][:attributes]).to have_key(:name)
+        expect(user_response[:data][:attributes]).to have_key(:email)
+        expect(user_response[:data][:attributes]).to have_key(:api_key)
+
+        expect(user_response[:data][:attributes][:name]).to be_a(String)
+        expect(user_response[:data][:attributes][:email]).to be_a(String)
+        expect(user_response[:data][:attributes][:api_key]).to be_a(String)
       end
     end
 
